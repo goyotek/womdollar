487cd6b98262ce726266bb02afe8e0034983ea3d
Greg Ohr <goyotek@gmail.com>
2014-04-14 21:32:24 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Womdollar::Application.config.secret_token = '92d799478fdfee30f1efb3faa39ca7a0bb927f5624f79fa9f317f1da8ff5b2b77979a337fdcf4b854b054d4a1d71517cb1c961f7e368723157fb736069b3b08c'
